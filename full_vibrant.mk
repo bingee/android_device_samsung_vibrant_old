eb24a8e6f4c2057a7ce7b9570987de2fa4a9786d
Benji Hertel <thedude@teamdouche.net>
2010-11-17 13:23:13 -0600

    Updates and fixes from codeworkx

---
+++ full_vibrant.mk
@@ -0,0 +1,114 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This is the product configuration for a generic GSM passion,
+# not specialized for any geography.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+PRODUCT_COPY_FILES += \
+    device/samsung/vibrant/init.smdkc110.rc:root/init.smdkc110.rc
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/vibrant/vibrant-vendor.mk)
+
+## (3) Finally, the least specific parts, i.e. the non-GSM-specific aspects
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240 \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libargs=-d /dev/ttyS0 \
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=15
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.startheapsize=8m \
+    dalvik.vm.heapsize=32m \
+    dalvik.vm.execution-mode=int:jit
+
+# Galaxy S uses high-density artwork where available
+PRODUCT_LOCALES := hdpi
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/vibrant/overlay
+
+# media profiles and capabilities spec
+$(call inherit-product, device/samsung/vibrant/media_a1026.mk)
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/samsung/vibrant/media_profiles.xml:system/etc/media_profiles.xml
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+
+# Keylayout / Keychars
+PRODUCT_COPY_FILES += \
+    device/samsung/vibrant/prebuilt/usr/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/samsung/vibrant/prebuilt/usr/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
+    device/samsung/vibrant/prebuilt/usr/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
+    device/samsung/vibrant/prebuilt/usr/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
+    device/samsung/vibrant/prebuilt/usr/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/samsung/vibrant/prebuilt/usr/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin 
+
+PRODUCT_COPY_FILES += \
+    device/samsung/vibrant/prebuilt/etc/asound.conf:system/etc/asound.conf \
+    device/samsung/vibrant/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
+    device/samsung/vibrant/prebuilt/lib/egl/egl.cfg:system/lib/egl/egl.cfg
+
+
+# Kernel
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#LOCAL_KERNEL := device/samsung/vibrant/kernel
+#else
+#LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_NAME := full_vibrant
+PRODUCT_DEVICE := vibrant
+PRODUCT_MODEL := SGH-T959
+PRODUCT_BRAND := Samsung
+PRODUCT_MANUFACTURER := Samsung
+TARGET_IS_GALAXYS := true
